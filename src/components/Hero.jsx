948956a964272b80ea3bf91ec29a1e9bed45c6bb
mihirkumarmeher-123 <mihirmeher22@gmail.com>
2025-03-10 09:55:03 +0530

    Updated project files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,10 @@
 
 /**
  *  Components
-*/
+ */
 
-import { ButtonPrimary,ButtonOutline } from "./Button";
+import { ButtonPrimary, ButtonOutline } from "./Button";
+import { ReactTyped } from "react-typed";
 
 function Hero() {
   return (
@@ -32,19 +33,28 @@ function Hero() {
             </div>
           </div>
           <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
-            Building Scalable Modern Websites for the Future
+            <span className="md:text-5xl sm:text-4xl text-3xl">Hi, there!</span><span className="md:text-5xl sm:text-4xl text-2xl font-bold">&#128075;</span> <br /> <span className="md:text-5xl sm:text-4xl text-3xl">I&apos;m </span>
+            <span className="highlight text-yellow-500 md:text-5xl sm:text-4xl text-3xl">
+              Mihir Kumar Meher
+            </span><br />
+            <ReactTyped className='md:text-4xl sm:text-3xl text-xl'
+                strings={['MERN Stack Developer','Full Stack Developer','MEAN Stack Developer','Open Source Contributor']}
+                typeSpeed={120}
+                backSpeed={140}
+                loop
+           />
           </h2>
           <div className="flex items-center gap-3">
-        <ButtonPrimary
-          href="https://drive.google.com/file/d/1ACBhsR9MDF8M5A78aZ8TpNuQ1xBpouf8/view?usp=drive_link" 
-          label="Download CV"
-          icon="download"
-          /> 
-          <ButtonOutline
-          href="#about"
-          label="Scroll down"
-          icon="arrow_downward"
-          />
+            <ButtonPrimary
+              href="https://drive.google.com/file/d/1ACBhsR9MDF8M5A78aZ8TpNuQ1xBpouf8/view?usp=drive_link"
+              label="Download CV"
+              icon="download"
+            />
+            <ButtonOutline
+              href="#about"
+              label="Scroll down"
+              icon="arrow_downward"
+            />
           </div>
         </div>
 
